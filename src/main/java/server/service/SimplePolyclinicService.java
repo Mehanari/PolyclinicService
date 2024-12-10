822ded9934da1a2a7281c0f0185053f0c9fcb668
Vladyslav Puzanov <102820106+Mehanari@users.noreply.github.com>
2024-12-11 00:58:09 +0200

    Added client faults handling.

--- src/main/java/server/service/SimplePolyclinicService.java
+++ src/main/java/server/service/SimplePolyclinicService.java
@@ -6,7 +6,11 @@ import entity.MedicalCard;
 import entity.ObjectFactory;
 import jakarta.jws.HandlerChain;
 import jakarta.jws.WebService;
+import jakarta.xml.soap.SOAPException;
+import jakarta.xml.soap.SOAPFactory;
+import jakarta.xml.soap.SOAPFault;
 import jakarta.xml.ws.Holder;
+import jakarta.xml.ws.soap.SOAPFaultException;
 import server.service.repository.InMemoryPolyclinicRepository;
 import server.service.repository.PolyclinicRepository;
 
@@ -33,7 +37,10 @@ public class SimplePolyclinicService implements PolyclinicService{
     }
 
     @Override
-    public int addMedicalCard(MedicalCard medicalCard, String clientToken, Holder<String> serverToken) {
+    public int addMedicalCard(MedicalCard medicalCard, String clientToken, Holder<String> serverToken) throws ServiceException {
+        if (medicalCard == null) {
+            throw new SOAPFaultException(createClientFault("Medical card is null"));
+        }
         MedicalCard addedCard = repository.addMedicalCard(medicalCard);
         return addedCard.getCardNumber();
     }
@@ -49,6 +56,9 @@ public class SimplePolyclinicService implements PolyclinicService{
 
     @Override
     public int addAppointmentForPatient(Appointment appointment, String clientToken, Holder<String> serverToken) throws ServiceException {
+        if (appointment == null) {
+            throw new SOAPFaultException(createClientFault("Appointment is null"));
+        }
         try {
             Appointment addedAppointment = repository.addAppointmentForPatient(appointment);
             return addedAppointment.getId();
@@ -60,6 +70,9 @@ public class SimplePolyclinicService implements PolyclinicService{
     @Override
     public Appointment updateAppointmentTime(int appointmentId, XMLGregorianCalendar startTime,
                                              XMLGregorianCalendar endTime, String clientToken, Holder<String> serverToken) throws ServiceException {
+        if (startTime == null || endTime == null) {
+            throw new SOAPFaultException(createClientFault("Start time or end time is null"));
+        }
         try {
             Appointment appointment = repository.getAppointment(appointmentId);
             appointment.setStartTime(startTime);
@@ -81,6 +94,9 @@ public class SimplePolyclinicService implements PolyclinicService{
 
     @Override
     public int addAppointmentResult(AppointmentResult appointmentResult, String clientToken, Holder<String> serverToken) throws ServiceException {
+        if (appointmentResult == null) {
+            throw new SOAPFaultException(createClientFault("Appointment result is null"));
+        }
         try {
             AppointmentResult addedResult = repository.addAppointmentResult(appointmentResult);
             return addedResult.getId();
@@ -91,10 +107,25 @@ public class SimplePolyclinicService implements PolyclinicService{
 
     @Override
     public AppointmentResult updateAppointmentResult(AppointmentResult appointmentResult, String clientToken, Holder<String> serverToken) throws ServiceException {
+        if (appointmentResult == null) {
+            throw new SOAPFaultException(createClientFault("Appointment result is null"));
+        }
         try {
             return repository.updateAppointmentResult(appointmentResult);
         } catch (Exception e) {
             throw new ServiceException(e.getMessage());
+        }
+    }
+
+    private SOAPFault createClientFault(String message) {
+        try {
+            String code = "Client";
+            SOAPFault fault = SOAPFactory.newInstance().createFault();
+            fault.setFaultString(message);
+            fault.setFaultCode(code);
+            return fault;
+        } catch (SOAPException e) {
+            throw new RuntimeException(e);
         }
     }
 }
